fc5373e38e6635bbce9cda5f90195023e3c5cc7a
KOSTA <KOSTA@KOSTA-HP>
2016-04-26 15:19:29 +0900

    sql

---
+++ HotelProject/WebContent/sql/hotel.sql
@@ -0,0 +1,31 @@
+create table customer(
+	id varchar2(100) primary key,
+	password varchar2(100) not null,
+	email varchar2(100) not null,
+	tel number not null
+);
+drop table hotel;
+create table hotel(
+	hotel_no number primary key,
+	price number not null,
+	info clob not null
+);
+
+create sequence hotel_seq;
+
+create table booking(
+   book_no number primary key,
+   hotel_no number not null,
+   id varchar2(100) not null,
+   startdate date not null,
+   enddate date not null,
+   constraint fk_hotel foreign key(hotel_no) references hotel(hotel_no),
+   constraint fk_customer foreign key(id) references customer(id)
+);
+
+create table picture(
+	picture_no number primary key,
+	hotel_no number not null,
+	name varchar2(100) not null,
+	constraint fk_hotel foreign key(hotel_no) references hotel(hotel_no)
+);

eb4006c564ae0bf7fedecd54cbfabe688ef6be35
KOSTA <KOSTA@KOSTA-HP>
2016-04-26 15:25:17 +0900

    sql

--- HotelProject/WebContent/sql/hotel.sql
+++ HotelProject/WebContent/sql/hotel.sql
@@ -22,10 +22,11 @@ create table booking(
    constraint fk_hotel foreign key(hotel_no) references hotel(hotel_no),
    constraint fk_customer foreign key(id) references customer(id)
 );
-
+create sequence booking_seq;
 create table picture(
 	picture_no number primary key,
 	hotel_no number not null,
 	name varchar2(100) not null,
 	constraint fk_picture foreign key(hotel_no) references hotel(hotel_no)
 );
+create sequence picture_seq;

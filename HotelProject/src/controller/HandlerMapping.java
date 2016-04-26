ee0efdd2dea4d5cb7d13e3d91390d320843ff045
kosta <kosta@kosta-HP>
2016-04-26 16:32:24 +0900

    handler/dispatcher

--- HotelProject/src/controller/HandlerMapping.java
+++ HotelProject/src/controller/HandlerMapping.java
@@ -1,5 +1,16 @@
 package controller;
 
 public class HandlerMapping {
-	
+	private static HandlerMapping instance = new HandlerMapping();
+
+	private HandlerMapping() {
+	}
+
+	public static HandlerMapping getInstance() {
+		return instance;
+	}
+	public Controller create(String command) {
+		Controller c = null;
+		return c;
+	}
 }

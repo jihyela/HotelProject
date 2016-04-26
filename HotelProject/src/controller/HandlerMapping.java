9dd1b29437186c0373e766a3e6c9f32381c71aa2
kosta <kosta@kosta-HP>
2016-04-26 16:19:20 +0900

    디스패쳐 핸들러매핑 노티스DAO

--- HotelProject/src/controller/HandlerMapping.java
+++ HotelProject/src/controller/HandlerMapping.java
@@ -1,5 +1,20 @@
 package controller;
 
 public class HandlerMapping {
-	
+	private static HandlerMapping instance = new HandlerMapping();
+
+	private HandlerMapping() {
+	}
+
+	public static HandlerMapping getInstance() {
+		return instance;
+	}
+
+	public Controller create(String command) {
+		Controller c = null;
+		/*if (command.equals("write")) {
+			c = new WriteController();
+		} */
+		return c;
+	}
 }

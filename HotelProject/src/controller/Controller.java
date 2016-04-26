73cef2cbc437a58c9fca962069ba76f582fa52c0
kosta <kosta@kosta-HP>
2016-04-26 15:22:39 +0900

    hi

--- HotelProject/src/controller/Controller.java
+++ HotelProject/src/controller/Controller.java
@@ -1,11 +1,11 @@
 package controller;
-
+//컨트롤러
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
 	public ModelAndView execute(HttpServletRequest request,
 			HttpServletResponse response) throws Exception;	
-	
+
 }
 

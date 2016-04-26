399dafab59e86dc308b183fe416274917f44f67d
kosta <kosta@kosta-HP>
2016-04-26 16:24:06 +0900

    customer

--- HotelProject/WebContent/sql/hotel.sql
+++ HotelProject/WebContent/sql/hotel.sql
@@ -1,10 +1,17 @@
 -- 회원정보 테이블
 create table customer(
    id varchar2(100) primary key,
+   name varchar2(100) not null,
    password varchar2(100) not null,
    email varchar2(100) not null,
    tel number not null
 );
+
+insert into customer(id, name, password, email, tel) values ('jihye', '라지혜', '1234', 'lajihye93@naver.com', '01089000843');
+
+select name from customer where id='jihye' and password='1234';
+
+drop table customer;
 drop table hotel;
 -- 객실정보 테이블
 create table hotel(
@@ -35,3 +42,25 @@ create table picture(
    constraint fk_picture foreign key(hotel_no) references hotel(hotel_no)
 );
 create sequence picture_seq;
+
+
+create table qna(
+   qna_no number primary key,
+   title varchar2(100) not null,
+      content clob not null,
+      time_posted date not null,
+      hits number default 0,
+      id varchar2(100) not null,
+      constraint fk_qna foreign key(id) references customer(id)
+);
+create sequence qna_seq;
+create table notice(
+   notice_no number primary key,
+   title varchar2(100) not null,
+      content clob not null,
+      time_posted date not null,
+      hits number default 0,
+      id varchar2(100) not null,
+      constraint fk_notice foreign key(id) references customer(id)
+);
+create sequence notice_seq;
--- HotelProject/src/model/CustomerDAO.java
+++ HotelProject/src/model/CustomerDAO.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 public class CustomerDAO {
@@ -7,5 +12,44 @@ public class CustomerDAO {
 	private DataSource dataSource;
 	private CustomerDAO() {
 		dataSource=DataSourceManager.getInstance().getDataSource();
+	}
+	public static CustomerDAO getInstance(){
+		return cdao;
+	}
+	public Connection getConnection() throws SQLException{
+		return dataSource.getConnection();
+	}
+	public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException{
+		if(pstmt!=null)
+			pstmt.close();
+		if(con!=null)
+			con.close(); 
+	}
+	public void closeAll(ResultSet rs,PreparedStatement pstmt,Connection con) throws SQLException{
+		if(rs!=null)
+			rs.close();
+		closeAll(pstmt,con);
+	}
+	
+	//login
+	public CustomerVO login(String id, String password) throws SQLException{
+		CustomerVO cvo = null;
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con = getConnection();
+			String sql = "select name, email, tel from customer where id=? and password=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				cvo = new CustomerVO(id, rs.getString(1), password, rs.getString(2), rs.getString(3));
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		return cvo;
 	}
 }

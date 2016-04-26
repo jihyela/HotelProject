6d4065268014fbc58616447d22e6e928969acd59
kosta <kosta@kosta-HP>
2016-04-26 15:58:36 +0900

    commit

--- HotelProject/src/model/DataSourceManager.java
+++ HotelProject/src/model/DataSourceManager.java
@@ -1,31 +1,56 @@
 package model;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 /**
- * Database Connection Pool ( DBCP )  
- * : 시스템 성능 향상을 위해 
- * 	 컨넥션을 생성 소멸하지 않고 
- *   미리 만들어 놓은 컨넥션을 빌려주고 반납받도록 한다. 
+ * 
+ *  컨넥션 풀을 생성하여 공유하는 객체
+ *  시스템 상에서 단 하나만 존재하면 되고
+ *  공유해서 사용할 수 있으면 된다. 
+ *  -> Singleton Pattern 
+ *
  */
 public class DataSourceManager {
 	private static DataSourceManager instance=new DataSourceManager();
-	private DataSource dataSource;
+	private DataSource ds;
 	private DataSourceManager(){
-		BasicDataSource dbcp=new BasicDataSource();
-		dbcp.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		dbcp.setUrl("jdbc:oracle:thin:@127.0.0.1:1521:xe");
-		dbcp.setUsername("scott");
-		dbcp.setPassword("tiger");
-		dbcp.setMaxActive(20);
-		dataSource=dbcp;
+		try {
+			InitialContext ctx=new InitialContext();
+			ds=(DataSource)ctx.lookup("java:comp/env/jdbc/mydbcp");
+			System.out.println("톰켓 데이터베이스 컨넥션 풀 생성 "+ds);
+		} catch (NamingException e) {			
+			e.printStackTrace();
+		}				
 	}
 	public static DataSourceManager getInstance(){
 		return instance;
 	}
 	public DataSource getDataSource(){
-		return dataSource;
+		return ds;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
